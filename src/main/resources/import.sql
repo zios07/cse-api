22c576fc84168e8f45c95db302b61321ba5aa748
Zakaria Kaoukab <Zkaoukab@omnidata.local>
2018-09-08 00:07:44 +0100

    fix sql syntax

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,15 @@
-insert into tb_location values (1, "RAB", "RABAT");
-insert into tb_location values (2, "CASA", "CASA BLANCA");
+insert into tb_location values (1, 'RAB', 'RABAT');
+insert into tb_location values (2, 'CASA', 'CASA BLANCA');
 
-insert into tb_subarea values (1, "SUB 1", 1999, 2888, 2);
-insert into tb_subarea values (2, "SUB 2", 1999, 2888, 2);
-insert into tb_subarea values (3, "SUB 3", 1999, 2888, 1);
-insert into tb_subarea values (4, "SUB 4", 1999, 2888, 1);
+insert into tb_subarea values (1, 'SUB 1', 1999, 2888, 2);
+insert into tb_subarea values (2, 'SUB 2', 1999, 2888, 2);
+insert into tb_subarea values (3, 'SUB 3', 1999, 2888, 1);
+insert into tb_subarea values (4, 'SUB 4', 1999, 2888, 1);
 
-insert into tb_type values (1, "HOME", "HOME");
-insert into tb_type values (2, "VILLA", "VILLA");
-insert into tb_type values (3, "APPAR", "APPARTMENT");
+insert into tb_type values (1, 'HOME', 'HOME');
+insert into tb_type values (2, 'VILLA', 'VILLA');
+insert into tb_type values (3, 'APPAR', 'APPARTMENT');
 
-insert into tb_property values (1, "Property 1 in an excellent state", 3, 4, 120, "REF#1", "Home of 123m", 1, 1);
-insert into tb_property values (2, "Property 1 in an excellent state", 3, 4, 120, "REF#1", "Villa of 123m", 4, 2);
-insert into tb_property values (3, "Property 1 in an excellent state", 3, 4, 120, "REF#1", "Appartment of 123m", 2, 3);
+insert into tb_property values (1, 'Property 1 in an excellent state', 3, 4, 120, 'REF#1', 'Home of 123m', 1, 1);
+insert into tb_property values (2, 'Property 1 in an excellent state', 3, 4, 120, 'REF#1', 'Villa of 123m', 4, 2);
+insert into tb_property values (3, 'Property 1 in an excellent state', 3, 4, 120, 'REF#1', 'Appartment of 123m', 2, 3);
